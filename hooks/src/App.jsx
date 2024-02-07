d5ae045de6845bb163805aa521d4afa9cb7d2850
Muhammad Abdullah <abdulx125@gmail.com>
2024-02-07 21:37:05 +0500

    learned hooks

---
+++ hooks/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  // let counter = 5;
+  // the variabel is changing in console log but not in dom we have to use useState now
+  let [counter, changingCounter] = useState(0);
+
+  const addValue = () => {
+    {
+      counter++;
+      changingCounter(counter);
+      console.log("clicked ", counter);
+    }
+  };
+
+  const removeValue = () => {
+    {
+      if (counter === 0) {
+        return (counter = 0);
+      }
+      counter--;
+      changingCounter(counter);
+    }
+  };
+  return (
+    <>
+      <h3>hooks in react</h3>
+      <h2>counter value = {counter}</h2>
+      <button onClick={addValue}>increment</button>
+      <br />
+      <button onClick={removeValue}>decrement</button>
+    </>
+  );
+}
+
+export default App;
